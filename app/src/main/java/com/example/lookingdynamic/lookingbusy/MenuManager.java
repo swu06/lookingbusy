160fc973a18bb9cdb42e874af372c4e98b1ba1ff
Sarah Wu <sarahjanewu@gmail.com>
2015-09-10 19:45:44 -0500

    Expanding Menu

--- app/src/main/java/com/example/lookingdynamic/lookingbusy/MenuManager.java
+++ app/src/main/java/com/example/lookingdynamic/lookingbusy/MenuManager.java
@@ -16,10 +16,18 @@ public class MenuManager {
     private static final String [] pausedMenuLabels = new String[] {"Themes", "GamePlay", "Restart Game", "Continue Current Game"};
     private static final Integer[] pausedMenuIcons = new Integer[] {R.drawable.popped_balloon, R.drawable.popped_droplet, R.drawable.popped_balloon, R.drawable.popped_droplet};
 
+    private static final String [] gamePlayMenuLabels = new String[] {"Relaxing", "Challanging"};
+    private static final Integer[] gamePlayMenuIcons = new Integer[] {R.drawable.popped_balloon, R.drawable.popped_droplet};
+
+    private static final String [] themeMenuLabels = new String[] {"Relaxing", "Challanging"};
+    private static final Integer[] themeMenuIcons = new Integer[] {R.drawable.popped_balloon, R.drawable.popped_droplet};
+
+
     private Context myContext;
     public MenuManager(Context myContext) {
         this.myContext = myContext;
     }
+
     public void showPausedMenu(final PopAllTheThingsGame game) {
         ListAdapter adapter = new ArrayAdapterWithIcons(myContext, pausedMenuLabels, pausedMenuIcons);
 
@@ -28,8 +36,44 @@ public class MenuManager {
         builder.setIcon(R.drawable.popped_ball);
         builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int item) {
+                if(item == 0) {
+                    showGamePlayMenu(game);
+                } else if(item == 3) {
+                    game.resume();
+                }
                 Log.d(LOGGER, "MenuClick Detected!, item# " + item);
-                game.resume();
+
+            }
+        });
+        builder.show();
+    }
+
+    public void showGamePlayMenu(final PopAllTheThingsGame game) {
+        ListAdapter adapter = new ArrayAdapterWithIcons(myContext, gamePlayMenuLabels, gamePlayMenuIcons);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(myContext);
+        builder.setTitle("Game Play Options");
+        builder.setIcon(R.drawable.popped_ball);
+        builder.setPositiveButton("OK",null);
+        builder.setSingleChoiceItems(adapter, 0, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                Log.d(LOGGER, "MenuClick Detected!, item# " + item);
+                showPausedMenu(game);
+            }
+        });
+        builder.show();
+    }
+
+    public void showThemeMenu(final PopAllTheThingsGame game) {
+        ListAdapter adapter = new ArrayAdapterWithIcons(myContext, gamePlayMenuLabels, gamePlayMenuIcons);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(myContext);
+        builder.setTitle("Game Play Options");
+        builder.setIcon(R.drawable.popped_ball);
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                Log.d(LOGGER, "MenuClick Detected!, item# " + item);
+                showPausedMenu(game);
             }
         });
         builder.show();
